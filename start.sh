0f33349fc09eba34b42ab7f07f69f637e5e7b04d
wwpry <83965467+wwpry@users.noreply.github.com>
2021-05-30 17:57:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,9 @@ yes "4" | bash status.sh c
 touch /root/.aria2/aria2.session
 chmod 0777 /root/.aria2/ -R
 
+
+nohup ./FolderMagic -aria "http://127.0.0.1:6800/jsonrpc" -auth root:$Aria2_secret -bind :9184 -root / -wd /webdav >> /dev/null 2>&1 & 
+
 mkdir /.config/
 mkdir /.config/rclone
 touch /.config/rclone/rclone.conf
@@ -17,13 +20,21 @@ wget git.io/tracker.sh
 chmod 0777 /tracker.sh
 /bin/bash tracker.sh "/root/.aria2/aria2.conf"
 
-git clone https://github.com/wwpry/bot-y
+
+
+git clone https://github.com/wwpry/bot-h
 mkdir /bot/
-mv /bot-y/bot/* /bot/
+mv /bot-h/bot/* /bot/
+#cp /bot-h/nginx.conf /etc/nginx/
+#cp /new/index.html /root/
+#chmod 0777 /root/index.html
 chmod 0777 /bot/ -R
-rm -rf /bot-y
+rm -rf /bot-h
+#python3 /bot/nginx.py
+#nginx -c /etc/nginx/nginx.conf
+#nginx -s reload
 
 nohup aria2c --conf-path=/root/.aria2/aria2.conf --rpc-listen-port=8080 --rpc-secret=$Aria2_secret &
-nohup python3 /bot/web.py &
+#nohup python3 /bot/web.py &
 
 python3 /bot/main.py
