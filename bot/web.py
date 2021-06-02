a7a1bbad5d902739c0914387f74b50d8203d9b66
wwpry <83965467+wwpry@users.noreply.github.com>
2021-06-02 21:53:52 +0800

    Update web.py

--- bot/web.py
+++ bot/web.py
@@ -56,4 +56,4 @@ def index():
 if __name__ == '__main__':
 
     port = int(os.environ.get('PORT'))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
